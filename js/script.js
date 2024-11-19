40442927b5a35dbc2049352e86870e92f7f9f7f7
MD Juman <mdjuman.developer.com>
2024-11-20 01:08:59 +0600

    add all section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  keyboard: {
-    enabled: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+let backTop = document.querySelector('.backtop');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 200) {
+    backTop.classList.add('show');
+    backTop.classList.remove('hide');
+  } else {
+    backTop.classList.add('hide');
+    backTop.classList.remove('show');
+  }
+});
+backTop.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 });
