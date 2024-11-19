900953b8c59b41700467837b39a871226685b293
MD Juman <mdjuman.developer.com>
2024-11-20 03:07:00 +0600

    add form

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ backTop.addEventListener('click', function () {
     behavior: 'smooth',
   });
 });
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav .navLink');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY; // স্ক্রল অবস্থান
+    let offset = sec.offsetTop - 150; // সেকশন শুরু
+    let height = sec.offsetHeight; // সেকশনের উচ্চতা
+    let id = sec.getAttribute('id'); // সেকশনের আইডি
+
+    // সেকশনটির মধ্যে স্ক্রল অবস্থান থাকলে
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active'); // আগের active ক্লাস মুছে ফেলো
+        document
+          .querySelector(`header nav a[href*='${id}']`) // সম্পর্কিত লিঙ্ক খুঁজে বের করো
+          .classList.add('active'); // নতুন active ক্লাস যোগ করো
+      });
+    }
+  });
+};
