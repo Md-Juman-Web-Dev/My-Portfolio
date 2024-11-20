a91660c7402b3ce4d7a403f193a65c7893cc1787
MD Juman <mdjuman.developer.com>
2024-11-21 02:29:27 +0600

    add dark mode

--- js/script.js
+++ js/script.js
@@ -16,46 +16,23 @@ backTop.addEventListener('click', function () {
   });
 });
 
-/* let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav .navLink');
-
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY; // স্ক্রল অবস্থান
-    let offset = sec.offsetTop - 150; // সেকশন শুরু
-    let height = sec.offsetHeight; // সেকশনের উচ্চতা
-    let id = sec.getAttribute('id'); // সেকশনের আইডি
-
-    // সেকশনটির মধ্যে স্ক্রল অবস্থান থাকলে
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove('active'); // আগের active ক্লাস মুছে ফেলো
-        document
-          .querySelector(`header nav a[href*='${id}']`) // সম্পর্কিত লিঙ্ক খুঁজে বের করো
-          .classList.add('active'); // নতুন active ক্লাস যোগ করো
-      });
-    }
-  });
-}; */
-
-/* let sideBar = document.querySelector('.sideber');
-
-document.addEventListener('click', function (event) {
-  const showSidbar = document.querySelector('.show');
-  if (sideBar.classList.contains(showSidbar)) {
-    if (!sideBar.event.target) {
-      sideBar.classList.remove('show');
-    }
-  }
-});
- */
 let sideBar = document.querySelector('.sideber');
 
 document.addEventListener('click', function (event) {
   if (sideBar.classList.contains('show')) {
-    // Check if the clicked target is outside the sidebar
     if (!sideBar.contains(event.target)) {
       sideBar.classList.remove('show');
     }
+  }
+});
+
+let body = document.querySelector('body');
+let darkMode = document.querySelector('.switch input');
+
+darkMode.addEventListener('change', function () {
+  if (darkMode.checked) {
+    body.classList.add('derkMode');
+  } else {
+    body.classList.remove('derkMode');
   }
 });
